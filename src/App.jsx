ed8fc31ef3d8a4304fa4cd7d7004972e401fd6ff
saiful230799 <mirajvai24@gmail.com>
2026-04-01 21:14:16 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import Navbar from './Components/NavBar/NavBar'
 import Banner from './Components/Banner/Banner'
 import Stats from './Components/Stats/Stats'
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { useState } from 'react'
 import MainContainer from './Components/MainContainer/MainContainer'
 import Steps from './Components/Steps/Steps'
@@ -17,40 +18,58 @@ function App() {
     const isExist = cart.find(item => item.id === product.id);
     if (!isExist) {
       setCart([...cart, product]);
+      toast.success("Added to cart", {
+        position: "top-right",
+        autoClose: 1500,
+      });
+    } else {
+      toast.warn("Product already in cart", {
+        position: "top-right",
+        autoClose: 1500,
+      });
     }
   };
 
   const handleRemove = (id) => {
     setCart(cart.filter(item => item.id !== id));
+    toast.error("Removed from cart", {
+      position: "top-right",
+      autoClose: 1500,
+    });
   };
 
   const handleCheckout = () => {
     setCart([]);
+    toast.info("Checkout successful", {
+      position: "top-right",
+      autoClose: 2000,
+    });
   };
 
   return (
-  <div className="scroll-smooth">
-    <Navbar cartCount={cart.length} /> 
-    <Banner />
-    <Stats />
-    <div id="products">
-      <MainContainer 
-        cart={cart} 
-        handleAddToCart={handleAddToCart} 
-        handleRemove={handleRemove} 
-        handleCheckout={handleCheckout} 
-      />
-    </div>
+    <div className="scroll-smooth">
+      <Navbar cartCount={cart.length} /> 
+      <Banner />
+      <Stats />
+      <div id="products">
+        <MainContainer 
+          cart={cart} 
+          handleAddToCart={handleAddToCart} 
+          handleRemove={handleRemove} 
+          handleCheckout={handleCheckout} 
+        />
+      </div>
 
-    <Steps /> 
-    <div id="pricing">
-      <Pricing /> 
-    </div>
+      <Steps /> 
+      <div id="pricing">
+        <Pricing /> 
+      </div>
 
-    <CtaSection /> 
-    <Footer />
-  </div>
-);
+      <CtaSection /> 
+      <Footer />
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
